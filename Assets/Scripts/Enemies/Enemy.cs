0711426fbbeb64c6d8216edae68aff445536eec5
Adria <adriapujolsierra@gmail.com>
2021-04-28 19:56:29 +0200

    Escena Alejo Updated. Cambios Prefab Character.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -5,10 +5,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float health = 100f;
+    public float invulnerabilityTime;
+    public float counter;
+
+    public void Awake()
+    {
+        counter = invulnerabilityTime;
+    }
 
     public void TakeDamage(float dmg)
     {
         health -= dmg;
+
+        if (counter > 0)
+        {
+            counter -= counter * Time.deltaTime;
+        }
+        else
+        {            
+            counter = invulnerabilityTime;
+        }
+
         if(health <= 0)
         {
             Destroy(gameObject);
